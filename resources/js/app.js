87198722e662ded6c2ca7bdea7d48e7dfdc729b8
ii <i.broshchak@gmail.com>
2023-03-04 10:00:43 +0200

    Add Vuetify styles and components and update app.js structure

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,16 +2,26 @@ import './bootstrap';
 import { createApp } from 'vue';
 
 import * as VueRouter from 'vue-router';
-import * as Vuetify from "vuetify";
-
-import 'vuetify/dist/vuetify.min.css';
-
 import App from './components/Tasks/Index.vue'
 
 const app = createApp(App)
 
-app.mount('#app')
-app.use(Vuetify)
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+
+
+app.use(vuetify)
 app.use(VueRouter)
+app.mount('#app')
+
+
 
 
