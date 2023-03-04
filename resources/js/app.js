6a9216f8cb2a65df7ab5e7cf08303ff6c620e668
ii <i.broshchak@gmail.com>
2023-03-04 09:18:31 +0200

    Install Vuetify and Vuerouter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,17 @@
 import './bootstrap';
 import { createApp } from 'vue';
 
+import * as VueRouter from 'vue-router';
+import * as Vuetify from "vuetify";
+
+import 'vuetify/dist/vuetify.min.css';
+
 import App from './components/Tasks/Index.vue'
 
 const app = createApp(App)
+
 app.mount('#app')
+app.use(Vuetify)
+app.use(VueRouter)
+
+
