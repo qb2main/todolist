21dbb9cd0dac70ed2ae4722839413f9bf9b6c3c1
ii <i.broshchak@gmail.com>
2023-03-04 10:01:05 +0200

    Add Talks base table

--- resources/js/components/Tasks/Index.vue
+++ resources/js/components/Tasks/Index.vue
@@ -1,3 +1,43 @@
 <template>
-    Table comming soon
+    <v-table>
+        <thead>
+        <tr>
+            <th class="text-left">
+                Task
+            </th>
+            <th class="text-left">
+                Actions
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr
+            v-for="item in desserts"
+            :key="item.name"
+        >
+            <td>{{ item.name }}</td>
+            <td>
+                <v-btn>
+                    Edit
+                </v-btn>
+            </td>
+        </tr>
+        </tbody>
+    </v-table>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            desserts: [
+                {
+                    name: 'Task1'
+                },
+                {
+                    name: 'Task2'
+                }
+            ],
+        }
+    },
+}
+</script>
