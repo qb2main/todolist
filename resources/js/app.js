8081e9f8ca4bce4792cf79fe135bafa395b4bc68
ii <i.broshchak@gmail.com>
2023-03-04 18:31:58 +0200

    Add mdi font

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,9 @@ import { createApp } from 'vue';
 import * as VueRouter from 'vue-router';
 import App from './components/Tasks/Index.vue'
 
+import 'vuetify/dist/vuetify.min.css';
+import '@mdi/font/css/materialdesignicons.css'
+
 const app = createApp(App)
 
 // Vuetify
@@ -14,6 +17,7 @@ import * as directives from 'vuetify/directives'
 const vuetify = createVuetify({
     components,
     directives,
+
 })
 
 
